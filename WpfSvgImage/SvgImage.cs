75465939ad8225a6558da5fd276c182e94599984
David Fischer <fischer1017@gmail.com>
2025-05-29 10:13:10 -0700

    Added .gitignore

--- WpfSvgImage/SvgImage.cs
+++ WpfSvgImage/SvgImage.cs
@@ -9,10 +9,18 @@ namespace WpfSvgImage
 {
 
     /// <summary>
-    /// A WPF Image control that displays SVG images
+    /// A WPF Image control that displays SVG images.
+    /// Provides static factory methods for creating instances from various SVG sources,
+    /// and a dependency property for binding to an SVG resource URI.
     /// </summary>
     public class SvgImage : Image
     {
+        /// <summary>
+        /// Creates a new <see cref="SvgImage"/> and sets its <see cref="SvgSourceUri"/> property.
+        /// The SVG will be loaded and rendered from the specified URI.
+        /// </summary>
+        /// <param name="svgSourceUri">The URI of the SVG resource.</param>
+        /// <returns>A new <see cref="SvgImage"/> instance.</returns>
         public static SvgImage CreateSvgImage(Uri svgSourceUri)
         {
             var svgImage = new SvgImage();
@@ -20,6 +28,13 @@ namespace WpfSvgImage
             return svgImage;
         }
 
+        /// <summary>
+        /// Creates a new <see cref="SvgImage"/> from a file path.
+        /// Loads and parses the SVG file, then renders it as an image.
+        /// </summary>
+        /// <param name="filePath">The file path to the SVG file.</param>
+        /// <returns>A new <see cref="SvgImage"/> instance.</returns>
+        /// <exception cref="InvalidOperationException">Thrown if the file does not contain a valid SVG document.</exception>
         public static SvgImage CreateSvgImage(string filePath)
         {
             var svgImage = new SvgImage();
@@ -33,6 +48,14 @@ namespace WpfSvgImage
             return svgImage;
         }
 
+        /// <summary>
+        /// Creates a new <see cref="SvgImage"/> from a stream.
+        /// Loads and parses the SVG data from the stream, then renders it as an image.
+        /// </summary>
+        /// <param name="svgStream">A stream containing SVG data.</param>
+        /// <returns>A new <see cref="SvgImage"/> instance.</returns>
+        /// <exception cref="ArgumentException">Thrown if the stream is null or empty.</exception>
+        /// <exception cref="InvalidOperationException">Thrown if the stream does not contain a valid SVG document.</exception>
         public static SvgImage CreateSvgImage(Stream svgStream)
         {
             var svgImage = new SvgImage();
@@ -52,6 +75,7 @@ namespace WpfSvgImage
 
         /// <summary>
         /// Gets or sets the URI of the SVG resource to display.
+        /// When set, the control loads, parses, and renders the SVG from the specified URI.
         /// </summary>
         public Uri SvgSourceUri
         {
@@ -60,7 +84,7 @@ namespace WpfSvgImage
         }
 
         /// <summary>
-        /// Identifies the SvgSourceUri dependency property.
+        /// Identifies the <see cref="SvgSourceUri"/> dependency property.
         /// </summary>
         public static readonly DependencyProperty SvgSourceUriProperty =
             DependencyProperty.Register(
@@ -70,10 +94,10 @@ namespace WpfSvgImage
                 new PropertyMetadata(null, SvgSourceChanged));
 
         /// <summary>
-        /// Called when the SvgSourceUri property changes.
-        /// Loads the SVG resource, parses it, and sets the Image Source.
+        /// Called when the <see cref="SvgSourceUri"/> property changes.
+        /// Loads the SVG resource from the specified URI, parses it, and sets the image source.
         /// </summary>
-        /// <param name="d">The dependency object (should be SvgImage).</param>
+        /// <param name="d">The dependency object (should be <see cref="SvgImage"/>).</param>
         /// <param name="args">The event arguments.</param>
         private static void SvgSourceChanged(DependencyObject d, DependencyPropertyChangedEventArgs args)
         {
